0504308c99dfdfaab3f52d2b9f06f873b77353c1
PredragBelos <predragbelos@gmail.com>
2023-10-22 13:50:56 +0200

    Change confic files

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    name: "Next starter",
+    title: "Next starter",
+    slogan: "Slogan za projekat",
+    description: "Opis projekta",
+    author: "Predrag Belos <predragbelos@gmail.com>",
+    logo: "Logo klijenta ili projekta",
+    imageShare: "",
+    facebookUrl: "Facebook klijenta",
+    instagramUrl: "Instagram klijenta",
+    linkedinUrl: "LinkedIn klijenta",
+    twitterUrl: "Twitter klijenta",
+    phone: "Telefon klijenta", // TODO
+    address: "Adresa klijenta",
+    region: "Vojvodina",
+    country: "Republika Srbija",
+    postalCode: "Poštanski broj klijenta",
+    locale: "sr",
+}
 
 module.exports = nextConfig
